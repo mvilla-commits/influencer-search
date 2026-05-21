868006492fd5ed88c66035822e8ec1785bb56eea
mvilla-commits <m.villa@cookiesagency.it>
2026-05-21 11:32:07 +0200

    Update search.js

--- functions/search.js
+++ functions/search.js
@@ -1,4 +1,16 @@
 exports.handler = async function(event, context) {
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
+      body: ""
+    };
+  }
+
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method not allowed" };
   }
